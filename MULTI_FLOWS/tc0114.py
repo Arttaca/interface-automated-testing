010a0114619e72310e1899d4207542ad24248654
Hieu <huyhung9630@gmail.com>
2024-09-20 11:26:01 +0700

    retest

--- MULTI_FLOWS/tc0114.py
+++ MULTI_FLOWS/tc0114.py
@@ -3,7 +3,7 @@ import seleniumbase
 import time
 import os
 
-def test_01_14(url):
+def test_01_14():
     with SB(
         extension_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'temp', 'metamask-chrome'))
         ) as sb:
