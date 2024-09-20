8f0aa1d78fa11b8103384f3e01d081becaae3c6f
LynkMAD <maianhduong16012004@gmail.com>
2024-09-20 21:21:49 +0700

    add click 3dot

--- MULTI_FLOWS/tc0107.py
+++ MULTI_FLOWS/tc0107.py
@@ -56,6 +56,7 @@ def test_01_07():
         sb.click('button:contains("Confirm")')
         sb.sleep(15)
         sb.switch_to_window(3)
+        sb.click(".three-dots")
         sb.click('button:contains("List Now")')
         sb.type('input#reservePrice', "0.0001")
         sb.type('input#quantity', "3")
